b19419b29ebf557eb823247fcf650ac3f1e249f0
Alexandre Gachet <alex.gachet@gmail>
2018-03-01 15:09:09 +0100

    Fix typo in index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_END === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
 	app.use(express.static('client/build'));
 	// Express will serve up the index.html file if it doesn't recognize the route
