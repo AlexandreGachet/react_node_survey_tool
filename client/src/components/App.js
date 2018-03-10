214f8076543221bc63269c48bdd34036273ee6d5
Alexandre Gachet <alex.gachet@gmail>
2018-03-10 15:34:07 +0100

    Users can see survey results

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,10 +4,9 @@ import { connect } from 'react-redux';
 
 import Header from './Header';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 import * as actions from '../actions';
-
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
 	componentDidMount() {
